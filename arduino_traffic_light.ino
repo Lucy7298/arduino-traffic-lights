740250bf0c69bbf6a4897e47dccc5f55bb101ff3
Yunxing <yunxingl@github.mit.edu>
2020-04-28 23:48:25 -0400

    final version

--- arduino_traffic_light.ino
+++ arduino_traffic_light.ino
@@ -1,61 +1,84 @@
 #include <ros.h>
-#include <light_controller/light_command.h>
+#include <std_msgs/UInt16.h>
 #include <Adafruit_NeoPixel.h>
  
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN_1            6
-#define PIN_2            5
- 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      64
 
-Adafruit_NeoPixel pixels_1 = Adafruit_NeoPixel(NUMPIXELS, PIN_1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels_2 = Adafruit_NeoPixel(NUMPIXELS, PIN_2, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel pixels_1 = Adafruit_NeoPixel(64, 2, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel pixels_2 = Adafruit_NeoPixel(64, 3, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel pixels_3 = Adafruit_NeoPixel(64, 5, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(64*4, 6, NEO_GRB + NEO_KHZ800);
 
 ros::NodeHandle  nh;
 
-void light_helper(uint8_t status, Adafruit_NeoPixel& pixels, const light_controller::light_command& command) {
-  pixels.clear(); 
-  for (int i = 0; i < NUMPIXELS; i++){
-    switch (status){
-      case command.EMPTY:
-          break; 
-      case command.RED:
-          if ((i/8)%4 == 2) { //rows
-              pixels.setPixelColor(i, pixels.Color(1, 0, 0));}         
-          break; 
-      case command.YELLOW:  
-          if (i%4 == 0) { //columns
-            pixels.setPixelColor(i, pixels.Color(1, 1, 0));     
-          }
-          break; 
-      case command.GREEN:   
-          if (i%5 == 0) { // no pattern 
-            pixels.setPixelColor(i, pixels.Color(0, 1, 0));    
-          } 
-          break; }
+const unsigned int red[4] PROGMEM = {33279, 45457, 46497, 65409}; 
+const unsigned int yellow[4] PROGMEM = {33279, 40337, 43413, 65409};  
+const unsigned int green[4] PROGMEM =  {33279, 35249, 38297, 65409}; 
+
+
+void light_helper(int light_msg, int count) { 
+  const unsigned int* light_pattern; 
+
+  switch (light_msg) {
+    case 1: 
+      light_pattern = red; 
+      break; 
+    case 2: 
+      light_pattern = yellow; 
+      break; 
+    case 3: 
+      light_pattern = green;  
+      break; 
+    case 0: 
+      return;  
   }
-  pixels.show(); 
+
+  unsigned int elt; 
+  for (byte i = 0; i < 4; ++i) {
+    elt = pgm_read_word_near(light_pattern + i); 
+    while (elt > 0) {
+      if (elt % 2) {
+        switch (light_msg){
+          case 1:
+              pixels.setPixelColor(count, pixels.Color(1, 0, 0));        
+              break; 
+          case 2:  
+              pixels.setPixelColor(count, pixels.Color(1, 1, 0));     
+              break; 
+          case 3:   
+              pixels.setPixelColor(count, pixels.Color(0, 1, 0)); 
+              break; 
+        }
+      }
+      elt = (unsigned int) elt >> 1; 
+      count++; 
+    }
+  }
+  
 }
 
-void messageCb( const light_controller::light_command& light_status){
-    light_helper(light_status.light1_status, pixels_1, light_status); 
-    light_helper(light_status.light2_status, pixels_2, light_status); 
+void messageCb( const std_msgs::UInt16& light_status){
+    pixels.clear();
+    unsigned int received = light_status.data; 
+    int next_status; 
+    for (int i = 0; i < 4; i++) {
+      next_status = received % 4; 
+      light_helper(next_status, 64*i); 
+      received = received >> 2; 
+      }
+    pixels.show(); 
   }
 
-ros::Subscriber<light_controller::light_command> sub("/light_controller/light_state", &messageCb );
+ros::Subscriber<std_msgs::UInt16> sub("/light_controller/light_state", &messageCb );
 
-int delayval = 10; // delay for half a second
- 
 void setup()
 {
-  pixels_1.begin(); // This initializes the NeoPixel library.
-  pixels_2.begin(); 
+  pixels.begin(); 
   nh.initNode(); 
   nh.subscribe(sub); //the name of the topic from ros_light_controller
 }
 
 void loop() {
   nh.spinOnce(); 
-  delay(delayval);
+  delay(10);
 }
